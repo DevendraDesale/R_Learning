3fb2b30e6d4e73f2df9598afa6b1e120c48374e9
Devendra Desale <devendra.desale@gmail.com>
2015-02-19 00:59:26 +0800

    Adding additional functions feature, like do.call and its implementation. In conditional testing added difference for & and &&, | and ||.

--- 4.WritingFunction.R
+++ 4.WritingFunction.R
@@ -28,3 +28,19 @@ timesN <- function(x, n=2)
   x * n
 }
 
+## Writing parametrised function with the default values.
+hello.person <- function(first,last='desale')
+{
+    sprintf("Hello %s %s",first,last)
+}
+## do.call trick will allows us to specify the name of a function either
+## as a character or as an object, and provide arguments as a list.
+
+do.call('hello.person', args = list('devendra','desale'))
+
+##  This is useful when building the function that allows user to specify an action.
+run.this <- function(x, func = mean){
+  do.call(func, args = list(x))
+}
+run.this(1:10)
+run.this(1:10,sum)
--- 5.Conditions.R
+++ 5.Conditions.R
@@ -19,13 +19,21 @@ if(k == 1){
 
 ifelse(1==1, 'Yes','NO')
 
+# Interesting looping structures with ifelse
+toTest <- c(1,0,0,1,1)
+ifelse(toTest == 1, 'Yes','No')
 
 
+## The double form (&& or ||) is best used in if and the single form (& or |) 
+## is necessary for ifelse. The double form compares only one element from each side,
+## while the single form compares each element of each side.”
 
+a <- c(1,1,0,1)
+b <- c(2,1,0,1)
 
+ifelse(a ==1 & b == 1, 'yes','no')
 
-
-
+ifelse(a ==1 && b ==1, 'yes','no')
 
 
 
