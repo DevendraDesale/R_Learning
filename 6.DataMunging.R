f6c6acb15ffa77d187f8d0ea727d6ef753882644
Devendra Desale <devendra.desale@gmail.com>
2015-02-20 01:24:56 +0800

    Adding **plr code which wasn't covered, also dataset for exploring joins.

--- 6.DataMunging.R
+++ 6.DataMunging.R
@@ -3,6 +3,7 @@ theMat
 apply(theMat, 1, sum)
 apply(theMat, 2, sum)
 
+# to deal with the NA use na.rm = TRUE argument.
 rowSums(theMat)
 df2 <- as.data.frame(theMat)
 rowSums(df2)
@@ -16,6 +17,22 @@ sapply(theList, sum)
 
 ?sapply
 
+firstList <- list(A = matrix(1:16,4), B = matrix(1:16,2),C = 1:5)
+secondList <- list(A = matrix(1:16,4), B = matrix(1:16,8),C = 15:1)
+
+## Test element-by-element whether they are identical
+mapply(identical,firstList,secondList)
+
+## build a simple function that adds the number of rows (or length) of
+## each corresponding element
+simpleFunc <- function(x, y)
+{
+  NROW(x) + NROW(y)
+}
+# apply the function to the two lists
+mapply(simpleFunc, firstList, secondList)
+
+
 library(ggplot2)
 data(diamonds)
 head(diamonds)
@@ -23,6 +40,13 @@ head(diamonds)
 
 ?aggregate
 aggregate(price ~ cut, diamonds,mean)
+
+## To group the data by more than one variable, add the additional variable
+## to the right side of the formula separating it with a plus sign (+).
+
+aggregate(price ~ cut + color, diamonds, mean)
+## To aggregate two variable use cbind on the left side of formula
+aggregate(cbind(price,color)~cut,diamonds,mean)
 
 
 
@@ -39,9 +63,114 @@ ddply(diamonds, "cut", nrow)
 ## Break in multiple datasets and pass summarize then provide additional  arg 
 ddply(diamonds, "cut", summarise, AvgPrice = mean(price))
 
-find average of caret in diamond
+# find average of caret in diamond
 
 ddply(diamonds, c('cut','color'), summarise, AvgPrice = mean(price), AvgCarat = mean(carat))
+
+
+## idata.frame is a good way to create ref to a data.frame, 
+## so that subsetting is faster & memory efficient.
+system.time(dlply(baseball,'id', nrow))
+ibaseball <- idata.frame(baseball)
+system.time(dlply(ibaseball,'id',nrow))
+
+require(data.table)
+
+
+theDF <- data.frame(A=1:10,
+                    B = letters[11:20],
+                    C = letters[11:20],
+                    D = rep(c("one",'two','three'),length = 10))
+
+theDT <- data.table(A=1:10,
+                    B = letters[11:20],
+                    C = letters[11:20],
+                    D = rep(c("one",'two','three'),length = 10))
+## The data.table creates index over every row like DB.
+theDF
+
+theDT
+
+## The default data.frame returns character factors. But data.table doesn't
+class(theDF$B)
+class(theDT$B)
+## Accessing the column data.
+theDF[,c('A','C')]
+## TO access columns we don't need character vectors.
+theDT[,list(A,C)]
+
+#Show Tables. shows all in-memory tables along with the size of each.
+tables()
+## We have to assign key to table explicitly for indexing.
+## to accomlpish this use setkey function
+setkey(theDT,D)
+# see how all elements with same keys are sorted together.
+theDT
+
+## to get the key for perticular data.table
+key(theDT)
+## If we again checked for the tables() we will get key for the table this time.
+tables()
+## using this you can speed up searches.
+theDT["one",]
+
+## One can create keys using multiple columns
+diamondsDT <- data.table(diamonds)
+setkey(diamondsDT,cut,color)
+## and use this for accessing the multiple columns
+
+diamondsDT[J("Ideal","E"),]
+## see we used J for joining the rows.
+?J
+
+## To get the aggregation of the data.
+aggregate(price~cut, diamonds, mean)
+
+## to do same with the data.table
+diamondsDT[, mean(price),by = cut]
+# To give name to the diamondDT result column
+diamondsDT[, list(price=mean(price)),by = cut]
+## one could include multiple column aggregations.
+diamondsDT[, list(price=mean(price)),by = list(cut,color)]
+diamondsDT[, list(price = mean(price), carat = mean(carat)),
+            by = list(cut, color)]
+
+## though one could easily use aggregate,**ply and other functions with data.table
+## it is advisable not to do so.
+
+
+## Downloading and unzipping a file using R
+download.file(url="http://jaredlander.com/data/US_Foreign_Aid.zip",
+               destfile="ForeignAid.zip")
+unzip("ForeignAid.zip")
+## load all data using for loop
+require(stringr)
+theDataFiles <- dir('data',pattern = '\\.csv')
+for(file in theDataFiles)
+{
+  # give name to file 
+  dataName <- str_sub(string = file, start = 12, end = 18)
+  ## read the csv using read.table
+  ## file.path is good way to specify a file name
+  ## Also use stringsAsFactor to avoid factoring the string, import as characters.
+  temp <-read.table(file= file.path("data",file),header=TRUE,sep = ',', 
+                    stringsAsFactors = FALSE)
+  assign(x= dataName, value=temp)
+  
+}
+
+Aid_90s00s <- merge(x = Aid_90s, y = Aid_00s, by.x = c("Country.Name","Program.Name"),
+                    by.y = c("Country.Name","Program.Name"))
+## merge can be pretty slower than its other alternatives.
+
+head(Aid_90s00s)
+
+## alternatively plyr could be used with 'join' function, but it requires that the 
+## column names should match for joining.
+
+require(plyr)
+Aid_90s00sPlr <- join(x = Aid_90s,y=Aid_00s,by = c('Country.Name','Program.Name'))
+head(Aid_90s00sPlr)
 
 #white  format -> long cast format 
 library(reshape2)
